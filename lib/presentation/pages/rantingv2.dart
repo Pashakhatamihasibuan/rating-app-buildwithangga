18db0cb75203df670dc7a4b3bf2d89e2ea271184
Pasha Khatami Hasibuan <pashakhatamihsb@gmail.com>
2024-09-09 20:56:08 +0700

    Penambahan textfield di file V2

--- lib/presentation/pages/rantingv2.dart
+++ lib/presentation/pages/rantingv2.dart
@@ -11,6 +11,7 @@ class Rantingv2 extends StatefulWidget {
 
 class _Rantingv2State extends State<Rantingv2> {
   List<int> selectedIndex = []; // Daftar untuk menyimpan bintang yang terpilih
+  final TextEditingController _messageController = TextEditingController();
 
   void _handleStarClick(int index) {
     setState(() {
@@ -25,106 +26,120 @@ class _Rantingv2State extends State<Rantingv2> {
   }
 
   @override
+  void dispose() {
+    _messageController.dispose(); // Dispose controller saat widget dihapus
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFFFFF),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 40,
-            left: 28,
-            right: 28,
-          ),
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/home-office.png",
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Text(
-                "Enjoy Your Meal",
-                style: GoogleFonts.poppins(
-                  color: const Color(0xFF121622),
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 40,
+              left: 28,
+              right: 28,
+            ),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/home-office.png",
                 ),
-              ),
-              const SizedBox(
-                height: 6,
-              ),
-              Text(
-                "Please rate our experience",
-                style: GoogleFonts.poppins(
-                  color: const Color(0xFF808EAB),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: List.generate(5, (index) {
-                  return StarsWidget(
-                    index: index,
-                    imagesUrl: "assets/icons/star2.png",
-                    selectedIndex: selectedIndex,
-                    onclick:
-                        _handleStarClick, // Menggunakan callback yang benar
-                  );
-                }),
-              ),
-              const SizedBox(
-                height: 36,
-              ),
-              Container(
-                width: 319,
-                height: 130,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(17),
-                  color: const Color(0xFFF8F8f8),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 16,
-                    left: 16,
+                Text(
+                  "Enjoy Your Meal",
+                  style: GoogleFonts.poppins(
+                    color: const Color(0xFF121622),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
                   ),
-                  child: Text(
-                    "Your Message",
-                    style: GoogleFonts.poppins(
-                      color: const Color(0xFF808EAB),
-                      fontWeight: FontWeight.w400,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  "Please rate our experience",
+                  style: GoogleFonts.poppins(
+                    color: const Color(0xFF808EAB),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(5, (index) {
+                    return StarsWidget(
+                      index: index,
+                      imagesUrl: "assets/icons/star2.png",
+                      selectedIndex: selectedIndex,
+                      onclick:
+                          _handleStarClick, // Menggunakan callback yang benar
+                    );
+                  }),
+                ),
+                const SizedBox(
+                  height: 36,
+                ),
+                Container(
+                  width: 319,
+                  height: 130,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(17),
+                    color: const Color(0xFFF8F8f8),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _messageController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Your Message',
+                        hintStyle: GoogleFonts.poppins(
+                          color: const Color(0xFF808EAB),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      maxLines: null, // Agar bisa multiline
+                      keyboardType: TextInputType.multiline,
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                width: 319,
-                height: 55,
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                      backgroundColor: const Color(0xFF4074E6),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(13),
-                      )),
-                  child: Text(
-                    "Submit Review",
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 319,
+                  height: 55,
+                  child: TextButton(
+                    onPressed: () {
+                      // Logika untuk submit review dan teks yang diinputkan
+                      print('Review: ${_messageController.text}');
+                    },
+                    style: TextButton.styleFrom(
+                        backgroundColor: const Color(0xFF4074E6),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(13),
+                        )),
+                    child: Text(
+                      "Submit Review",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
